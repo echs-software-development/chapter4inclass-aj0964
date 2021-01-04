062109ca5a06fd576389996b54574015aefd3279
whack <aj0964@students.necsd.net>
2021-01-04 15:20:20 +0000

    prob 1 2 & 3(3 has errors)

--- Prob1.java
+++ Prob1.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 
 public class Prob1 {
   public static void main(String[] args){
+  Scanner scan = new Scanner(System.in);
+  System.out.print("Enter a positive interger: ");
+
+  int num = scan.nextInt();
+  System.out.println((int)( num * Math.random()) + 1 );  System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );    System.out.println((int)( num * Math.random()) + 1 );  
 
   }
 }
